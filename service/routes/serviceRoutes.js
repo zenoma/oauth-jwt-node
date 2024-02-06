07e2f5f0d2bb9b7c61b680ea084adfffd2211f06
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-02-06 14:53:19 +0100

    Change JWTVerify to be an async function

--- service/routes/serviceRoutes.js
+++ service/routes/serviceRoutes.js
@@ -1,34 +1,33 @@
 import express from 'express';
-import {verifyJWT} from '../../utils/jwtUtils.js';
+import { verifyJWT } from '../../utils/jwtUtils.js';
 import SimpleLogger from '../../utils/simpleLogger.js';
 
 const logger = new SimpleLogger();
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
   const authorizationHeader = req.headers.authorization;
 
   if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-      logger.log('Token not provided');
-      return res.status(401).json({ message: 'Token not provided' });
+    logger.log('Token not provided');
+    return res.status(401).json({ message: 'Token not provided' });
   }
 
   const token = authorizationHeader.split(' ')[1];
+  try {
+    const decoded = await verifyJWT(token);
 
-    if (token) {
-      verifyJWT(token, (err, decoded) => {
-        if (err) {
-          logger.log(`Invalid token`)
-          return res.status(401).json({ message: 'Invalid Token' });
-        }
-  
-        logger.log(`Valid token usage for ${decoded.login}`)
-        res.json({ message: "Data fetched", user: decoded.login });
-      });
-    } else {
-      res.status(401).json({ message: 'Token not provided' });
+    if(!decoded){
+      throw new Error("Invalid token")
     }
-  });
+
+    logger.log(`Valid token usage for ${decoded.login}`);
+    return res.json({ message: 'Data fetched', user: decoded.login });
+    
+  } catch (error) {
+    logger.log(`Invalid token`);
+    return res.status(401).json({ message: 'Invalid Token' });
+  }
+});
 
 export default router;
--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,15 +1,31 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import SimpleLogger from './simpleLogger.js';
 
 dotenv.config();
+const logger = new SimpleLogger();
 
-const secret_phrase = process.env.SECRET_PHRASE;
+const SECRET_PHRASE = process.env.SECRET_PHRASE;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
-export const signJWT = (data) => {
-  return jwt.sign(data, secret_phrase);
+export const signJWT = (data) => {  
+  const expiresIn = process.env.JWT_EXPIRES_IN || '1h';
+
+  return jwt.sign(data, SECRET_PHRASE, { expiresIn});
 };
 
-export const verifyJWT = (token, callback) => {
-  jwt.verify(token, secret_phrase, callback);
+export const verifyJWT = async (token) => {
+  if (!token) {
+    logger.log('No token provided');
+    return false;
+  }
+
+  try {
+    const decoded = await jwt.verify(token, SECRET_PHRASE);
+    return decoded;
+  } catch (error) {
+    logger.log('Invalid token');
+    return false;
+  }
 };
 
